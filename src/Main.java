39eaba8f371ef54ca8fbd6c69864c9d19db8e2d8
EricLi <ericl521@nycstudents.net>
2025-11-20 11:40:59 -0500

    Finsihed WordMatch

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        WordMatch w = new WordMatch("mississippi");
-        System.out.println(w.scoreGuess("i"));
-        System.out.println(w.scoreGuess("iss"));
-        System.out.println(w.scoreGuess("issipp"));
-        System.out.println(w.scoreGuess("mississippi"));
-
-        WordMatch game = new WordMatch("concatenation");
-        System.out.println(game.scoreGuess("ten"));
-        System.out.println(game.scoreGuess("nation"));
-        System.out.println(game.findBetterGuess("ten", "nation"));
-        System.out.println(game.scoreGuess("con"));
-        System.out.println(game.scoreGuess("cat"));
-        System.out.println(game.findBetterGuess("con", "cat"));
-
+    public static void main(String[] args) throws IOException {
+//        WordMatch w = new WordMatch("mississippi");
+//        System.out.println(w.scoreGuess("i"));
+//        System.out.println(w.scoreGuess("iss"));
+//        System.out.println(w.scoreGuess("issipp"));
+//        System.out.println(w.scoreGuess("mississippi"));
+//
+//        WordMatch game = new WordMatch("concatenation");
+//        System.out.println(game.scoreGuess("ten"));
+//        System.out.println(game.scoreGuess("nation"));
+//        System.out.println(game.findBetterGuess("ten", "nation"));
+//        System.out.println(game.scoreGuess("con"));
+//        System.out.println(game.scoreGuess("cat"));
+//        System.out.println(game.findBetterGuess("con", "cat"));
+        System.out.println(read());
     }
-
-
+    public static int read () throws IOException {
+        int points = 0;
+        File f = new File("Guesses.txt");
+        Scanner s = new Scanner(f);
+        while (s.hasNextLine()) {
+            WordMatch w = new WordMatch(s.next());
+            points += w.scoreGuess(w.findBetterGuess(s.next(), s.next()));
+        }
+        return points;
+    }
 }
--- src/WordMatch.java
+++ src/WordMatch.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 public class WordMatch {
     private String secret;
     public WordMatch(String word){
@@ -5,18 +9,19 @@ public class WordMatch {
     }
     public int scoreGuess(String guess){
         int score = 0;
-        for(int i = 0; i <= secret.length()-guess.length(); i++){
+        for(int i = 0; i <= secret.length() - guess.length(); i++){
             if (secret.substring(i, i + guess.length()).equals(guess)){
                 score++;
             }
-
         }
-        return score * guess.length()* guess.length();
-
+        return score * guess.length() * guess.length();
     }
     public String findBetterGuess(String guess1, String guess2){
         if(scoreGuess(guess1) > scoreGuess(guess2)){
             return guess1;
+        }
+        if(scoreGuess(guess2) > scoreGuess(guess1)){
+            return guess2;
         }
         if(guess1.compareTo(guess2) > 0) return guess1;
         return guess2;
